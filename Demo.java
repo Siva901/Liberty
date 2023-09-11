bbdc025fdab79e7a6b2f5aab4bf0773ce4c85240
Siva Jothi <89371435+Siva901@users.noreply.github.com>
2023-09-11 20:58:49 +0530

    Assignment1
    completed

---
+++ Demo.java
@@ -0,0 +1,27 @@
+package day1.sample;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to la_liberty");
+		
+		int x = 5;//number integer 
+		int y = 99;
+		int z = x+y;
+		System.out.println("The Value of two number is :" +z);
+		
+		String sName = "Liberty";
+		System.out.println(sName);
+		
+		boolean bName = true;
+		System.out.println(bName);
+		
+		float	fname = 34.5f;
+		System.out.println(fname);
+		
+		char	ch = 'L';
+		System.out.println(ch);
+	}
+
+}
